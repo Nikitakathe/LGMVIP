85621b1c22a14f264ffd325c43fdb71b8d28e061
Nikitakathe <150245524+Nikitakathe@users.noreply.github.com>
2023-12-26 15:48:47 +0530

    Update ScientificCalculator.java

--- Java-Task-3/src/ScientificCalculator.java
+++ Java-Task-3/src/ScientificCalculator.java
@@ -1,1 +1,22 @@
-j
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+/**
+ *
+ * @author NIKITA KATHE
+ */
+public class ScientificCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
